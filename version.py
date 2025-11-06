49e8b817acf20cf84c200bcc1d561e918595be7c
GianniPicocli <gianni@ussolutions.it>
2025-11-06 15:45:10 +0100

    chore: release 1.0.9

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """Informazioni dedicate alla versione dell'applicazione."""
 
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 # Manifesto aggiornamenti pubblicato nel repository remoto (branch main)
 UPDATE_MANIFEST_URL = "https://raw.githubusercontent.com/GianniUSS/AppPremi/main/update_manifest.json"
 
